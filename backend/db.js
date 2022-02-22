5767a178b42a97429c4dede205dec633d9e0464d
manishtomarleo21 <manishtomarleo21@gmail.com>
2022-02-22 19:55:28 +0530

    Added mongoDB to our local database

---
+++ backend/db.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+const mongoUrl = 'mongodb://localhost:27017/?readPreference=primary&appname=MongoDB%20Compass&ssl=false';
+
+const connectToMongo = ()=>{
+    mongoose.connect(mongoUrl, ()=>{
+        console.log("Connect to MongoDB");
+    })
+}
+
+module.exports = connectToMongo;
