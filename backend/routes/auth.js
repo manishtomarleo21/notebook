4262ce0df2ce4e883a03074e360dddb9d0261b1c
manishtomarleo21 <manishtomarleo21@gmail.com>
2022-02-23 00:32:54 +0530

    Created login endpoints and sending authentication GWT token

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -50,8 +50,54 @@ router.post('/createuser',[
     }
     catch (error) {
        console.error(error.message);
-       res.status(500).send('Some error occured') 
+       res.status(500).send('Internal server error occured') 
     }
 })
+
+
+
+//Login authentication using: POST '/api/auth/login'. No login required
+router.post('/login',[
+    body('email', 'Enter a valid email.').isEmail(),
+    body('password', 'Password cannot be blank.').exists(),
+    // body('password', 'Password must be at least of 5 characters.').isLength({ min: 5 }),
+], async (req, res)=>{
+    //If there are errors in validations send bad request
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    const{email, password} = req.body;
+    try {
+        let user = await User.findOne({email})
+        //if user mail in incorrect
+        if(!user){
+            return res.status(500).send({error:"Please try to login with correct mail ID."})
+        }
+        let passwordCompare = await bcrypt.compare(password, user.password);
+        //if user mail in incorrect
+        if(!passwordCompare){
+            return res.status(500).send({error:"Please try to login with credentials."})
+        }
+
+        //if both correct
+        const data = {
+            user:{
+                id:user.id
+            }
+        } 
+        const authToken = jwt.sign(data, JWT_SECRET);
+        
+        //sending the authentication token
+        res.json({authToken})
+        
+
+
+    } catch (error) {
+        console.error(error.message);
+        res.status(500).send('Internal server error occured') 
+    }
+})
+
 
 module.exports = router
